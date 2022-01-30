6cdbd1b7ca92b7f52204ab7f96c6bba1c4b490c5
Igor Garbuz <kinetikapotencia@gmail.com>
2022-01-31 01:57:03 +0300

    setup default redux

---
+++ client/src/redux/store/store.js
@@ -0,0 +1,14 @@
+import {applyMiddleware, createStore} from "redux";
+import {rootReducer} from "../reducers/rootReducer";
+import createSagaMiddleware from "redux-saga"
+import {rootSaga} from "../sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(
+    sagaMiddleware
+));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
