2824e067e52106c2d728f74edf74a2bcee61015c
Igor Garbuz <kinetikapotencia@gmail.com>
2022-02-13 12:57:47 +0300

    created mock of login form

--- client/src/components/Auth/LoginForm/index.js
+++ client/src/components/Auth/LoginForm/index.js
@@ -1,12 +1,92 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import useStyle from './styles'
+import {Formik} from 'formik'
+import {useNavigate} from "react-router-dom";
+import {Button, IconButton, InputAdornment, TextField, Typography} from "@mui/material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import {POST} from "../../../helpers/HTTPMethods";
+import routes from "../../../router/routes";
 
 const LoginForm = (props) => {
     const classes = useStyle();
 
-    return (
-        <div>
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const navigate = useNavigate();
 
+    const handleVisibilityChange = useCallback(() => {
+        setIsPasswordVisible(prev => !prev);
+    }, [setIsPasswordVisible]);
+
+    const handleSubmit = useCallback((values, {setSubmitting}) => {
+        console.log("COMPLEX API CALL");
+        navigate(routes.landing);
+    }, []);
+
+    const handleNavigationToRegisterPage = useCallback(() => {
+        navigate(routes.register);
+    }, []);
+
+    return (
+        <div className={classes.loginSection}>
+            <Typography variant={"h4"} className={classes.loginHeader}>Login</Typography>
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: ''
+                }}
+                onSubmit={handleSubmit}
+            >
+                {({getFieldProps, handleSubmit}) => (
+                    <>
+                        <form onSubmit={handleSubmit} method={POST} className={classes.loginForm}>
+                            <TextField
+                                fullWidth
+                                className={classes.input}
+                                variant={"outlined"}
+                                label={"Email"}
+                                placeholder={"Write your email here"}
+                                type={"email"}
+                                name={"email"}
+                                {...getFieldProps('email')}
+                            />
+                            <TextField
+                                fullWidth
+                                className={classes.input}
+                                variant={"outlined"}
+                                label={"Password"}
+                                placeholder={"Write your password here"}
+                                type={isPasswordVisible ? "text" : "password"}
+                                {...getFieldProps('password')}
+                                InputProps={{
+                                    endAdornment: <InputAdornment position={"end"}>
+                                        <IconButton
+                                            onClick={handleVisibilityChange}
+                                        >
+                                            {isPasswordVisible ? <VisibilityOff/> : <Visibility/>}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                }
+                            />
+                            <Button
+                                variant={"contained"}
+                                size={"large"}
+                                type={"submit"}
+                                className={classes.submitButton}>
+                                Log in
+                            </Button>
+                        </form>
+                    </>
+                )}
+            </Formik>
+            <div className={classes.registerHint}>
+                <Typography variant={"subtitle1"}>
+                    Don't have an account yet?
+                </Typography>
+                <Button variant={"text"} size={"large"} onClick={handleNavigationToRegisterPage}>
+                    Sign up
+                </Button>
+            </div>
         </div>
     );
 };
@@ -14,5 +94,11 @@ const LoginForm = (props) => {
 LoginForm.propTypes = {
 
 };
+
+//isPasswordVisible: PropTypes.bool.isRequired,
+//     handleVisibilityChange: PropTypes.func.isRequired
+//isPasswordVisible={isPasswordVisible}
+//     handleVisibilityChange={handleVisibilityChange}
+
 
 export default LoginForm;
--- client/src/components/Auth/LoginForm/styles.js
+++ client/src/components/Auth/LoginForm/styles.js
@@ -2,7 +2,35 @@ import {makeStyles} from "@mui/styles";
 
 
 const useStyle = makeStyles({
-
+    loginSection: {
+        padding: "20px 30px",
+        borderRadius: "12px",
+        display: "flex",
+        flexDirection: "column",
+        zIndex: 10,
+        backgroundColor: "white"
+    },
+    loginHeader: {
+        marginBottom: "20px"
+    },
+    input: {
+        marginBottom: "10px",
+        "&:last-child" : {
+            marginBottom: "0px"
+        }
+    },
+    submitButton: {
+        marginTop: "30px"
+    },
+    loginForm: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    registerHint: {
+        paddingTop: "20px",
+        display: "flex",
+        flexDirection: "column"
+    }
 });
 
 export default useStyle;
